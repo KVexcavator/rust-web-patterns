c97b500c1e72f470617e802e37a3eda5ae19c056
Excavator <kvronin@gmail.com>
2026-03-02 16:03:28 +0500

    add custom error

---
+++ 05-http-requests/project/nanoservices/to_do/core/src/api/basic_actions/create.rs
@@ -0,0 +1,14 @@
+use crate::enums::TaskStatus;
+use crate::structs::ToDoItem;
+
+#[cfg(feature = "json-file-storage")]
+use to_do_dal::json_file::save_one;
+
+pub fn create(title: &str, status: TaskStatus) -> Result<ToDoItem, String> {
+    let item = ToDoItem {
+        title: title.to_string(),
+        status,
+    };
+    let _ = save_one(&title.to_string(), &item)?;
+    Ok(item)
+}
--- 05-http-requests/project/to_do/core/src/api/basic_actions/get.rs
+++
@@ -1,8 +0,0 @@
-use to_do_dal::json_file::get_all as get_all_handle;
-use crate::structs::{ToDoItem, AllToDoItems};
-
-pub async  fn get_all() -> Result<AllToDoItems, String> {
-  Ok(AllToDoItems::from_hashmap(
-    get_all_handle::<ToDoItem>()?
-  ))
-}
