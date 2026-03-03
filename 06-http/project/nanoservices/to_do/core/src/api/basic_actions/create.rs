ffa112b883655ebcf77b9e1c84deab3bfcb364e3
Excavator <kvronin@gmail.com>
2026-03-03 14:34:48 +0500

    06 project: impl delete url

--- 06-http/project/nanoservices/to_do/core/src/api/basic_actions/create.rs
+++ 06-http/project/nanoservices/to_do/core/src/api/basic_actions/create.rs
@@ -1,7 +1,7 @@
 use crate::structs::ToDoItem;
+use glue::errors::NanoServiceError;
 #[cfg(feature = "json-file-storage")]
 use to_do_dal::json_file::save_one;
-use glue::errors::NanoServiceError;
 
 pub async fn create(item: ToDoItem) -> Result<ToDoItem, NanoServiceError> {
     let _ = save_one(&item.title.to_string(), &item)?;
--- 06-http/project/readme.txt
+++ 06-http/project/readme.txt
@@ -14,3 +14,4 @@ curl -X POST http://127.0.0.1:8080/api/v1/create \
 -H "Content-Type: application/json" \
 -d '{"title": "writing", "status": "PENDING"}'
 
+curl -X DELETE http://127.0.0.1:8080/api/v1/delete/writing
